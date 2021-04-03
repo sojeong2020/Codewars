0187463d141b951595e046127eb0fa1c36962692
sojeong2020 <73712403+sojeong2020@users.noreply.github.com>
2021-04-03 14:20:23 +0100

    .

---
+++ 8-kyu/Even-odd-number.js
@@ -0,0 +1,14 @@
+//Create a function (or write a script in Shell) that takes an integer as an argument 
+//and returns "Even" for even numbers or "Odd" for odd numbers.
+
+//My Solution
+
+function even_or_odd(number) {
+  if( number % 2 == 0 ){
+    return "Even";
+  }else {
+    return "Odd";
+  }
+}
+
+// use remainder
