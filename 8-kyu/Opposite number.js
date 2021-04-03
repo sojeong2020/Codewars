5afad968bf8445c27b5c114c2b304c51d3d940c4
sojeong2020 <73712403+sojeong2020@users.noreply.github.com>
2021-04-03 16:59:51 +0100

    .

---
+++ 8-kyu/Opposite number.js
@@ -0,0 +1,22 @@
+//Very simple, given a number, find its opposite.
+
+//Examples:
+
+//1: -1
+//14: -14
+
+
+//My solution
+
+function opposite(number) {
+  return -number;
+}
+
+//easy
+
+
+
+//Other solution from Codewars
+
+//const opposite = number => -number;
+
