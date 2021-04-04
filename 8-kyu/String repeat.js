346fc2d0c3978282f68c4a57a4419c1ebeb9582d
sojeong2020 <73712403+sojeong2020@users.noreply.github.com>
2021-04-04 23:42:43 +0100

    .

---
+++ 8-kyu/String repeat.js
@@ -0,0 +1,21 @@
+//Write a function called repeat_string which repeats the given string str exactly count times.
+
+//repeatStr(6, "I") // "IIIIII"
+//repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+
+//My Solution
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
+
+
+//Other solutions from Codewars
+
+//solution 1
+
+repeatStr = (n, s) => s.repeat(n)
+
+
+
