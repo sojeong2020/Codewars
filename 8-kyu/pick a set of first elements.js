06bd51c6ba8db8ca01c0b7b8a9e7a21d652283fd
sojeong2020 <73712403+sojeong2020@users.noreply.github.com>
2021-04-15 22:53:49 +0100

    .

---
+++ 8-kyu/pick a set of first elements.js
@@ -0,0 +1,24 @@
+/*Write a function to get the first elements of asequence. Passing a parameter n (default=1) will return the first n elements of the sequence.
+
+If n == 0 return an empty sequence []
+
+Examples
+var arr = ['a', 'b', 'c', 'd', 'e'];
+first(arr) //=> ['a'];
+first(arr, 2) //=> ['a', 'b']
+first(arr, 3) //=> ['a', 'b', 'c'];
+first(arr, 0) //=> [];
+*/
+
+//My solution
+
+function first(arr, n) {
+  let newArr=[];
+  if(n=== undefined){
+    newArr.push(arr[0])
+  return newArr;
+  }
+ 
+     return arr.slice(0,n);
+  
+}
